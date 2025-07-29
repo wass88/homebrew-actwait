cf26dbedde3daba8d6f56cfe3d1a3292944f22e4
wass88 <wasss80@gmail.com>
2025-07-29 15:52:18 +0900

    actwait 1.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/actwait.rb
+++ Formula/actwait.rb
@@ -1,8 +1,8 @@
 class RebaseStack < Formula
   desc      "Automated stacked Git rebase helper"
   homepage  "https://github.com/wass88/actwait"
-  url       "https://github.com/wass88/actwait/archive/v0.1.0.tar.gz"
-  sha256    "wip"
+  url       "https://github.com/wass88/actwait/archive/refs/tags/v1.0.0.tar.gz"
+  sha256    "3fdc7a34c9c1dc27fa106314e1a6cf9945baa659f4889ea6f2af65f5bb5b0b44"
   license   "MIT"
 
   def install
